9880e529114b6e9c4816695468e35ce5e6f70542
Aqeel <aahl0002@student.monash.edu>
2020-05-06 16:37:35 +0800

    chore/Fixed issue with cards

--- cholesterol/app/src/main/java/com/example/cholesterol/MainActivity.java
+++ cholesterol/app/src/main/java/com/example/cholesterol/MainActivity.java
@@ -24,19 +24,7 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.example.cholesterol.APIRequest;
 
 /*
 * Android: 9.0 (Pie)
@@ -67,13 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recycler_view);
 
-        CholesterolAdapter cholesterolAdapter = new CholesterolAdapter(cholesterol);
+//        CholesterolAdapter cholesterolAdapter = new CholesterolAdapter(cholesterol);
 
-        recyclerView.setAdapter(cholesterolAdapter);
+//        recyclerView.setAdapter(cholesterolAdapter);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
         tv = findViewById(R.id.tv);
     }
@@ -83,68 +71,70 @@ public class MainActivity extends AppCompatActivity {
         EditText keyword;
         keyword = findViewById(R.id.editText);
         String patientID = keyword.getText().toString();
-
-
-
-
-//        makeRequest(patientID);
+        makeRequest(patientID);
 
     }
 
-//        private void makeRequest(String keywords) {
-//            RequestQueue queue = Volley.newRequestQueue(this);
-//            String url = "https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/Observation?_count=13&code=2093-3&patient=" + keywords + "&_sort=date&_format=json";
-//
-//            JsonObjectRequest stringRequest =
-//                    new JsonObjectRequest(Request.Method.GET, url, null,
-//                            new Response.Listener<JSONObject>() {
-//                                @Override
-//                                public void onResponse(JSONObject response) {
-//                                    try {
-////                                      Create an array of patient items
-//                                        ArrayList<Patient> patients = new ArrayList<>();
-//                                        double cholValue;
-//                                        Patient patient;
-//                                        String chol;
-//                                        String CholLevel;
-//                                        String unitValue;
-//                                        JSONArray entry = response.getJSONArray("entry");
-//                                        for (int i = 0; i < entry.length(); i++) {
-//                                            try {
-//                                                cholValue = entry.getJSONObject(i).getJSONObject("resource").getJSONObject("valueQuantity").getDouble("value");
-//                                                unitValue = entry.getJSONObject(i).getJSONObject("resource").getJSONObject("valueQuantity").getString("unit");
-//                                                chol = String.valueOf(cholValue);
-//
-//                                                CholLevel = chol + unitValue;
-//
-//                                                patient = new Patient(chol);
-//
-////                                              The cholesterol Levels will be added to this ArrayList
-//                                                cholesterol.add(CholLevel);
-//
-//                                                patients.add(patient);
-//
-////                                              This is the sout:
-//                                                Log.d("chol", String.valueOf(patients));
-//                                            } catch (Exception e) {
-//                                            }
-//                                        }
-//                                    } catch (Exception e) {
-//
-//                                    }
-//                                }
-//                            }, new Response.ErrorListener() {
-//
-//                        @Override
-//                        public void onErrorResponse(VolleyError error) {
-//                            Log.d("stock", error.getMessage());
-//                        }
-//                    });
-//            stringRequest.setRetryPolicy(new DefaultRetryPolicy(
-//                    50000,
-//                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-//                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-//
-//            queue.add(stringRequest);
-//        }
+        private void makeRequest(String keywords) {
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/Observation?_count=13&code=2093-3&patient=" + keywords + "&_sort=date&_format=json";
+
+            JsonObjectRequest stringRequest =
+                    new JsonObjectRequest(Request.Method.GET, url, null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+//                                      Create an array of patient items
+                                        ArrayList<Patient> patients = new ArrayList<>();
+                                        double cholValue;
+                                        Patient patient;
+                                        String chol;
+                                        String CholLevel;
+                                        String unitValue;
+                                        JSONArray entry = response.getJSONArray("entry");
+                                        for (int i = 0; i < entry.length(); i++) {
+                                            try {
+                                                cholValue = entry.getJSONObject(i).getJSONObject("resource").getJSONObject("valueQuantity").getDouble("value");
+                                                unitValue = entry.getJSONObject(i).getJSONObject("resource").getJSONObject("valueQuantity").getString("unit");
+                                                chol = String.valueOf(cholValue);
+
+                                                CholLevel = chol + unitValue;
+
+                                                patient = new Patient(chol);
+
+//                                              The cholesterol Levels will be added to this ArrayList
+                                                cholesterol.add(CholLevel);
+
+                                                patients.add(patient);
+
+//                                              This is the sout:
+                                                Log.d("chol", String.valueOf(patients));
+                                            } catch (Exception e) {
+                                            }
+                                        }
+
+                                        CholesterolAdapter cholesterolAdapter = new CholesterolAdapter(cholesterol);
+                                        recyclerView.setAdapter(cholesterolAdapter);
+
+
+
+                                    } catch (Exception e) {
+
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("stock", error.getMessage());
+                        }
+                    });
+            stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    50000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+            queue.add(stringRequest);
+        }
 }
